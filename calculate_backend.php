72ef1fc37d8642b46ba441e9bae1463ea097aeac
maks20000 <31661972+maks20000@users.noreply.github.com>
2020-01-10 19:56:42 +0300

    Update calculate_backend.php

--- calculate_backend.php
+++ calculate_backend.php
@@ -17,30 +17,30 @@
     $orderSum = $get_data['data']["orderSum"];
     $response;
 
-   // if ($product!="") putInFile($weight, $width, $height, $length, $product, $price);
-    
-    $dalli = new DalliService ("ed9fc57846ed6ef8331be2710ee930de");
+/**    
+    $dalli = new DalliService ("");
     $dalli->SetParam($weight,$regionName,$endCity,$width,$height,$length, $price, $orderSum);
     $response['pvz'][]=$dalli->GetPrice($dalli->typePVZ);
     $response['kur'][]=$dalli->GetPrice($dalli->typeKUR);
 
-    $boxberry = new BoxberryService("https://api.boxberry.ru/json.php", "c17dc20337e4ba9d62cbed56f6a84736");
+    $boxberry = new BoxberryService("https://api.boxberry.ru/json.php", "");
     $boxberry->setParam($weight,$endCity,$width,$height,$length, $price, $orderSum);
     $response['pvz'][]=$boxberry->GetDeliveryCosts(false);
     $response['kur'][]=$boxberry->GetDeliveryCosts(true);
 
-    $postal = new Postal ("0HO0Zkq2mDqlAAm7GjXfU1Qp2X4tMmt6", "ZC1rYXJwaWtpQHlhbmRleC5jb206UG9jaHRAMjAxOQ==");
+    $postal = new Postal ("", "");
     $postal->SetParam ($weight, $endCity, $width, $height, $length, $price, $orderSum);
     $response['post'][] = $postal->GetPrice("POSTAL_PARCEL");
     $response['post'][] = $postal->GetPrice("EMS");
     
 
 
-    $cdek = new cdekService("2d3hGnJWn949P8gncPt1vVVvTApZKxDm", "aGpYuJxNRT9OhDcLFAxVJCi9CBug765a");
+    $cdek = new cdekService("", "");
     $cdek->SetParam($weight, $regionCode, $endCity, $width, $height, $length, $price, $orderSum);
     $result = $cdek->GetPrice();
     if ($result!=false) {
         $r = json_encode(array_merge(json_decode($result, true),$response));
         echo $r;
     } else echo json_encode($response);
+    **/
 ?>
